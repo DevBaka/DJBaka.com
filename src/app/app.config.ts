c3e5b88a541e4c808101d549d2a13c6b84ecf4b4
DJBaka <devbaka@protonmail.com>
2025-02-18 06:31:01 +0100

    remove markdown

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routeConfig } from './app.routes';
-import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routeConfig), provideMarkdown()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routeConfig)]
 };
