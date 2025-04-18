ce8b9d3bbb6efb96b50b604ef4cd4aae59f3975b
DJBaka <devbaka@protonmail.com>
2025-04-18 23:31:44 +0200

    add animations to social links

--- src/app/components/columnes/columnes-links/columnes-links.component.ts
+++ src/app/components/columnes/columnes-links/columnes-links.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component,Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-columnes-links',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './columnes-links.component.html',
   styleUrl: './columnes-links.component.scss'
 })
-export class ColumnesLinksComponent {
+export class ColumnesLinksComponent implements AfterViewInit{
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      if (typeof IntersectionObserver !== 'undefined') {
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+              observer.unobserve(entry.target);
+            }
+          });
+        }, { threshold: 0.1 });
+
+        const elements = document.querySelectorAll('.fade-in');
+        elements.forEach((el) => observer.observe(el));
+      } else {
+        const elements = document.querySelectorAll('.fade-in');
+        elements.forEach((el) => el.classList.add('visible'));
+      }
+    }
+  }
+  
 }
