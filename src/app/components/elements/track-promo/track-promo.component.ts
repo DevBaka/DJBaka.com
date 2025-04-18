27a74b876adebccf2cef3636088f87a486e4068f
DJBaka <devbaka@protonmail.com>
2025-04-19 01:09:19 +0200

    übergabeparameter für track-promo

--- src/app/components/elements/track-promo/track-promo.component.ts
+++ src/app/components/elements/track-promo/track-promo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FadeInOnScrollDirective } from '../../../shared/fade-in-on-scroll.directive';
 
 @Component({
@@ -9,5 +9,11 @@ import { FadeInOnScrollDirective } from '../../../shared/fade-in-on-scroll.direc
   styleUrl: './track-promo.component.scss'
 })
 export class TrackPromoComponent {
-
+  @Input() title!: string;
+  @Input() subtitle!: string;
+  @Input() description1!: string;
+  @Input() description2!: string;
+  @Input() buttonText: string = 'Jetzt Anhören';
+  @Input() buttonLink!: string;
+  @Input() imageUrl!: string;
 }
