5aeeeffbda71dfbffaca5bb982e00a6a5631a186
DJBaka <devbaka@protonmail.com>
2025-04-19 02:20:27 +0200

    more animations

--- src/app/components/elements/section-block/section-block.component.ts
+++ src/app/components/elements/section-block/section-block.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FadeInOnScrollDirective } from '../../../shared/fade-in-on-scroll.directive';
 
 @Component({
   selector: 'app-section-block',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FadeInOnScrollDirective],
   templateUrl: './section-block.component.html',
   styleUrls: ['./section-block.component.scss']
 })
