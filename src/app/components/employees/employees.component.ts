e4b19d04a7a0ad17aec6ae7098f2c70ec50297ba
DJBaka <devbaka@protonmail.com>
2025-06-07 04:27:57 +0200

    add data to db with api

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../service/data.service';
 import { response } from 'express';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Employee } from '../../models/employee';
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss'
 })
 export class EmployeesComponent implements OnInit {
   employees:any;
+  employee = new Employee;
 
   constructor(private dataService:DataService){
 
@@ -24,6 +27,12 @@ export class EmployeesComponent implements OnInit {
     this.dataService.getData().subscribe(res => {
       this.employees = res;
     });
+  }
+
+  insertData(){
+    this.dataService.insertData(this.employee).subscribe(res =>{
+      this.getEmployeesData();
+    })
   }
 
 }
